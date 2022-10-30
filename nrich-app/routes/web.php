412d7a634071604c4990174a674ac943a7bc3c14
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-10-30 09:44:47 +0900

    レイアウト作成

--- nrich-app/routes/web.php
+++ nrich-app/routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\CircleController@index');
 
+Route::resource('admin', App\Http\Controllers\AdminController::class);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
