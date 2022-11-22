6c5bec493cc6c9af26390478edf8b494f2a3a383
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-11-22 11:04:35 +0900

    見た目の調整、文字数制限の追加

--- nrich-app/database/migrations/2022_10_30_005315_create_circles_table.php
+++ nrich-app/database/migrations/2022_10_30_005315_create_circles_table.php
@@ -16,17 +16,17 @@ return new class extends Migration
         Schema::create('circles', function (Blueprint $table) {
             $table->id(); //サークルID
             $table->integer('university_id'); //大学ID
-            $table->string('name'); //サークル名
-            $table->string('catchphrase'); //キャッチフレーズ
+            $table->string('name', 32); //サークル名
+            $table->string('catchphrase', 32); //キャッチフレーズ
             $table->text('summary'); //概要
             $table->integer('number_of_men'); //男子メンバー人数
             $table->integer('number_of_women'); //女子メンバー人数
             $table->string('location', 64); //活動場所
             $table->string('frequency', 64); //活動日数
-            $table->string('membership_fee'); //入会費
-            $table->string('admission_fee'); //会費
-            $table->string('initial_cost'); //初期費用
-            $table->string('number_of_camps'); //合宿回数
+            $table->string('membership_fee', 64); //入会費
+            $table->string('admission_fee', 64); //会費
+            $table->string('initial_cost', 64); //初期費用
+            $table->string('number_of_camps', 64); //合宿回数
             $table->integer('seriousness'); //真剣さ
             $table->integer('relationship'); //仲間
             $table->integer('experience');//経験
